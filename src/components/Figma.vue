e97fd6e169351f95d779d5d2b7a9191247585625
Rezvan Farz Kanari <rezvan.kanari@nl.abnamro.com>
2022-12-21 15:43:51 +0100

    update figma

--- src/components/Figma.vue
+++ src/components/Figma.vue
@@ -2,7 +2,7 @@
 import { defineProps } from 'vue';
 const urls = {
   'address-book': 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FThd7a8QfFW7eIr1GggrhVO%2FIB-Rebuild-Address-book%3Fnode-id%3D2%253A3444%26t%3DDTin2TyVMO6quRxT-1',
-  'mutation-overview': 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fx3YX0WARE3ExCoftkw3btf%2FIB-Rebuild%3Fnode-id%3D4336%253A220%26t%3D4u5cIZTgzpjRuRbX-1'
+  'mutation-overview': 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fx3YX0WARE3ExCoftkw3btf%2FIB-Rebuild%3Fnode-id%3D14527%253A152372%26t%3DMILcbNoYpJUTY8IX-1'
 };
 defineProps(['application']);
 </script>
