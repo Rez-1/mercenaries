3f3e50ef9f0f3fb56446e96dd0e359b56864f75f
C86892 <raju.adla@nl.abnamro.com>
2022-12-21 13:57:13 +0100

    updated enriched buttons

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div id="form" class="container">
     <div class="form_row row mt-2" >
       <div class="col-4">
-        <label for="application" class="text-right">Select Application</label>
+        <label for="application" class="text-right align-middle">Select Application</label>
       </div>
       <div class="col-8">
         <select name="application" id="application" class="form-select" v-model="application">
@@ -20,20 +20,21 @@
     </div>
     <div class="form_row row mt-2" >
       <div class="col-4">
-        <label for="language" class="text-right">Select Language</label>
+        <label for="language" class="text-right align-middle">Select Language</label>
       </div>
       <div class="col-8">
-        <select name="language" id="language" class="form-select" v-model="language">
-          <option :value="''">
-            Select...
-          </option>
-          <option
-            v-for="lang in languages"
-            :key="lang"
-            :value="lang.value">
-              {{lang.name}}
-          </option>
-        </select>
+        <div class="d-flex">
+          <div v-for="lang in languages" :key="lang" class="me-2">
+            <input
+            type="radio"
+            class="btn-check"
+            name="options"
+            @click="selectLang(lang.value)"
+            :checked="selectedLanguage(lang.value)"
+            :id="lang.value" autocomplete="off">
+            <label class="btn btn-primary" :for="lang.value">{{lang.name}}</label>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -42,9 +43,16 @@
 
 <script setup>
 import TranslationTable from './components/TranslationTable.vue'
-import {ref} from 'vue';
+import { ref } from 'vue';
 const application = ref('ibrs_address_book');
 const language = ref('en');
+
+function selectLang(val) {
+  language.value = val;
+}
+
+const selectedLanguage = (val) => (val === language.value);
+
 const applications = ref([
   {
     name: 'Address Book',
