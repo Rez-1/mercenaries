6ab426d9fe1187ee242f82e86e9af857f5c8a337
C86892 <raju.adla@nl.abnamro.com>
2022-12-21 12:55:15 +0100

    added bootstrap styling updated table styles added application and language selection

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,78 @@
 <template>
-  <TranslationTable />
+<div id="form" class="container">
+    <div class="form_row row mt-2" >
+      <div class="col-4">
+        <label for="application" class="text-right">Select Application</label>
+      </div>
+      <div class="col-8">
+        <select name="application" id="application" class="form-select" v-model="application">
+          <option :value="''">
+            Select...
+          </option>
+          <option
+            v-for="application in applications"
+            :key="application"
+            :value="application.value">
+              {{application.name}}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="form_row row mt-2" >
+      <div class="col-4">
+        <label for="language" class="text-right">Select Language</label>
+      </div>
+      <div class="col-8">
+        <select name="language" id="language" class="form-select" v-model="language">
+          <option :value="''">
+            Select...
+          </option>
+          <option
+            v-for="lang in languages"
+            :key="lang"
+            :value="lang.value">
+              {{lang.name}}
+          </option>
+        </select>
+      </div>
+    </div>
+  </div>
+  <TranslationTable v-if="application && language"/>
 </template>
 
 <script setup>
 import TranslationTable from './components/TranslationTable.vue'
+import {ref} from 'vue';
+const application = ref('ibrs_address_book');
+const language = ref('en');
+const applications = ref([
+  {
+    name: 'Address Book',
+    value: 'ibrs_address_book',
+  },
+  {
+    name: 'Mutations',
+    value: 'ibrs_mutations',
+  },
+  {
+    name: 'Pensions',
+    value: 'ibrs_pensions',
+  }
+]);
+const languages = ref([
+  {
+    name: 'English',
+    value: 'en',
+  },
+  {
+    name: 'Dutch',
+    value: 'nl',
+  }
+])
 </script>
+<style scoped>
+@import'~bootstrap/dist/css/bootstrap.css';
+label{
+  font-weight: bold;
+}
+</style>
--- src/components/TranslationTable.vue
+++ src/components/TranslationTable.vue
@@ -37,14 +37,38 @@ function save() {
 </script>
 
 <template>
-  <input type="text" placeholder="filter by key" name="filter" v-model="filterTerm">
-  <ul>
+<div class="container">
+  <div class="float-right mt-3 col-2">
+    <input class="form-control" type="text" placeholder="filter by key" name="filter" v-model="filterTerm">
+  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col" style="width: 30%">Key</th>
+        <th scope="col">Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="key in filteredTranslationsKeys" :key="key">
+        <th scope="row">
+          <label :for="key">{{ key }}</label></th>
+        <td>
+          <textarea :id="key" class="form-control" type="text" :name="key" v-model="internalObj[key]" />
+          <p v-if="internalObj[key] !== translations[key]" style="color: orange">
+            old value: {{  translations[key] }}
+          </p>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <!-- <ul>
     <li v-for="key in filteredTranslationsKeys" :key="key">
       {{ key }} <input type="text" :name="key" v-model="internalObj[key]" />
       <span v-if="internalObj[key] !== translations[key]" style="color: orange">
         old value: {{  translations[key] }}
       </span>
     </li>
-  </ul>
-  <button @click="save">Save</button>
+  </ul> -->
+  <button @click="save" class="btn btn-success" type="button">Save</button>
+</div>
 </template>
